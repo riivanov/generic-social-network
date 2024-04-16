d5ac96cdfe5e05a392103eb87e376eb2639e8e5b
Radoslav Ivanov <riivanov63@gmail.com>
2024-04-16 08:40:50 +0300

    :alembic:(auth): add onClick handler to return SWAPI response for C3PO

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -3,6 +3,14 @@
 import styles from "./page.module.scss";
 
 export default function Login() {
+  async function clicked() {
+    // call facade / service to make API call
+    // supabase ? make my own API?
+    const url = `https://swapi.dev/api/people/2`;
+    const res = await fetch(url);
+    console.log(await res.json())
+  }
+
   return (
     <>
       <div className={styles.container}>
@@ -13,6 +21,7 @@ export default function Login() {
           <label>Password:</label>
           <input id="pass" className="pass" type="password" />
         </form>
+        <button onClick={clicked}>Login</button>
       </div>
     </>
   );
