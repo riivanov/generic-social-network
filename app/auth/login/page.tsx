355bab79dd1f0f8de6043f970c2bb84de25e881e
Radoslav Ivanov <riivanov63@gmail.com>
2024-04-16 08:39:15 +0300

    :lipstick:(auth): add <label> for each <input>; some styles

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,12 +1,17 @@
+'use client'
+
 import styles from "./page.module.scss";
 
 export default function Login() {
   return (
     <>
-      <div className="container">
-        <form className="form">
-          <input className="user" type="text" />
-          <input className="pass" type="password" />
+      <div className={styles.container}>
+        <form className={styles.form}>
+          {/* TODO(auth): custom input field component */}
+          <label>Username:</label>
+          <input id="user" className="user" type="text" />
+          <label>Password:</label>
+          <input id="pass" className="pass" type="password" />
         </form>
       </div>
     </>
