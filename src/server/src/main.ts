54ebdf56c6c9f2e9b9d81dd569f902885b7715c6
Radoslav Ivanov <riivanov63@gmail.com>
2024-05-12 16:10:38 +0300

    :loud_sound:(server): add server log; store port in var (needs .env entry)

--- src/server/src/main.ts
+++ src/server/src/main.ts
@@ -1,8 +1,17 @@
+import { ConsoleLogger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const logger = new ConsoleLogger('app', {
+  logLevels: ['log'],
+  timestamp: true,
+});
+
+const port = 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  logger.log(`Server listening on ${port}`)
 }
 bootstrap();
