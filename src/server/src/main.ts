c83965ec1859e257e69fdd18c499eff80fae27c4
Radoslav Ivanov <riivanov63@gmail.com>
2024-05-17 15:57:35 +0300

    :technologist:(server): read PORT environment variable from .env file

--- src/server/src/main.ts
+++ src/server/src/main.ts
@@ -1,4 +1,5 @@
 import { ConsoleLogger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -7,11 +8,14 @@ const logger = new ConsoleLogger('app', {
   timestamp: true,
 });
 
-const port = 3000;
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // TODO(rivanov): iterate and introduce a custom interface;
+  // See: https://docs.nestjs.com/techniques/configuration#using-the-configservice
+  const svcConfig = app.get(ConfigService);
+  const port = svcConfig.get<string>('PORT');
+
   await app.listen(port);
-  logger.log(`Server listening on ${port}`)
+  logger.log(`Server listening on ${port}`);
 }
 bootstrap();
