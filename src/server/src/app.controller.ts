794e8d23993f077e159b185d6ec8691d44389018
Radoslav Ivanov <riivanov63@gmail.com>
2025-01-15 14:43:41 +0200

    :passport_control:(server): add /api/v1/user route for Create from CRUD

--- src/server/src/app.controller.ts
+++ src/server/src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+
+@Controller('/api/v1/')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  //
+  // CRUD routes for User
+  //
+
+  @Post('user')
+  createUser() {
+    return this.appService.createUser();
   }
 }
