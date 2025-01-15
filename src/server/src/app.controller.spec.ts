b7154f23587719ad4fe4bb263003b006d07acb6b
Radoslav Ivanov <riivanov63@gmail.com>
2025-01-15 15:44:50 +0200

    :white_check_mark:(server): test the /user route with no DB

--- src/server/src/app.controller.spec.ts
+++ src/server/src/app.controller.spec.ts
@@ -6,17 +6,29 @@ describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
+    const app: TestingModule =
+      await Test.createTestingModule({
+        controllers: [AppController],
+        providers: [AppService],
+      }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBe(
+        'Hello World!',
+      );
+    });
+
+    // CRUD routes for User
+    it('/api/v1/user', () => {
+      expect.assertions(1);
+      expect(appController.createUser()).toStrictEqual({
+        username: 'Joe',
+        password: 'password',
+      });
     });
   });
 });
