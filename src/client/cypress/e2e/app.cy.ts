ad47c1c39d8853ff55b1dec1591132ecb8a03cab
Radoslav Ivanov <riivanov63@gmail.com>
2025-01-21 16:55:31 +0200

    :hammer:(client): allow localhost to be referenced with /

--- src/client/cypress/e2e/app.cy.ts
+++ src/client/cypress/e2e/app.cy.ts
@@ -2,7 +2,7 @@
 
 describe('gsn client', () => {
   beforeEach(() => {
-    cy.visit(`http://localhost:3000`)
+    cy.visit(`/`)
   })
   it('has a Login button', () => {
     const button = cy.get(`body > nav > button`);
