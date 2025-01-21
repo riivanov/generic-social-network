3146e31140d3aab330d1c3adcde83201392adda6
Radoslav Ivanov <riivanov63@gmail.com>
2025-01-21 13:31:32 +0200

    :white_check_mark:(client): test button redirect to /auth/login
    will break once I style the page

--- src/client/cypress/e2e/app.cy.ts
+++ src/client/cypress/e2e/app.cy.ts
@@ -8,4 +8,11 @@ describe('gsn client', () => {
     const button = cy.get(`body > nav > button`);
     button.should(`have.text`, `Login`);
   })
+  it('redirects to /auth/login when Login button is clicked', () => {
+    const button = cy.get(`body > nav > button`);
+    button.click();
+    cy.url().should('include', '/auth/login')
+    cy.get('body > div > form > label:nth-child(1)').should('have.text', 'Username:')
+    cy.get('body > div > form > label:nth-child(3)').should('have.text', 'Password:')
+  })
 })
