40386c9d3993335fba50bf97f6191b28fd7b3dba
Radoslav Ivanov <riivanov63@gmail.com>
2025-01-31 15:12:10 +0200

    :white_check_mark:(server): make test pass; needs refactoring eventually

--- src/server/src/app.controller.spec.ts
+++ src/server/src/app.controller.spec.ts
@@ -1,15 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
 
 describe('AppController', () => {
   let appController: AppController;
+  let svcUser: UsersService;
 
   beforeEach(async () => {
     const app: TestingModule =
       await Test.createTestingModule({
         controllers: [AppController],
-        providers: [AppService],
+        providers: [AppService, UsersService],
       }).compile();
 
     appController = app.get<AppController>(AppController);
@@ -30,7 +32,12 @@ describe('AppController', () => {
     // Create user
     it('should ceraate a user when /user is called', () => {
       expect.assertions(1);
-      expect(appController.createUser()).toStrictEqual({
+      expect(svcUser.createUser({
+        id: 1,
+        username: 'Joe',
+        password: 'password'
+      })).toStrictEqual({
+        id: 1,
         username: 'Joe',
         password: 'password',
       });
@@ -39,7 +46,7 @@ describe('AppController', () => {
     // Read user
     it('should return a JWT when /api/v1/auth/login is called', async () => {
       // expect.assertions(1);
-      const result = { id: 1, username: 'Joe' };
+      const result = { access_token: "" }
       jest
         .spyOn(appController, 'login')
         .mockImplementation(async () => result);
