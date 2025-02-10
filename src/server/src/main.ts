f890f1d7e40c8a8d73578b3d7e2b9bc40093ff58
Radoslav Ivanov <riivanov63@gmail.com>
2025-02-10 16:32:39 +0200

    :wastebasket:(server): remove comments

--- src/server/src/main.ts
+++ src/server/src/main.ts
@@ -1,4 +1,3 @@
-import { User } from '@lib/entity/User';
 import { ConsoleLogger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -18,24 +17,6 @@ async function bootstrap() {
 }
 bootstrap();
 
-AppDataSource.initialize()
-  // .then(async () => {
-  //   console.log(
-  //     'Inserting a new user into the database...',
-  //   );
-  //   // const user = {"firstName": "Joe", "lastName": "Blow", "age": 37 };
-  //   const user = new User();
-  //   user.username = 'Joe';
-  //   user.password = 'password';
-  //   await AppDataSource.manager.save(user);
-  //   console.log('Saved a new user with id: ' + user.id);
-
-  //   console.log('Loading users from the database...');
-  //   const users = await AppDataSource.manager.find(User);
-  //   console.log('Loaded users: ', users);
-
-  //   console.log(
-  //     'Here you can setup and run express / fastify / any other framework.',
-  //   );
-  // })
-  .catch((error) => console.log(error));
+AppDataSource.initialize().catch((error) =>
+  console.log(error),
+);
