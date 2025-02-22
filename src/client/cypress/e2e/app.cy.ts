5df1f06233cee092e39a560c36faf144b314d986
Radoslav Ivanov <riivanov63@gmail.com>
2025-02-22 17:52:30 +0200

    :white_check_mark:(client): add more cypress tests for auth & register routes

--- src/client/cypress/e2e/app.cy.ts
+++ src/client/cypress/e2e/app.cy.ts
@@ -1,18 +1,45 @@
 /// <reference types="cypress" />
 
-describe('gsn client', () => {
+describe("gsn client", () => {
   beforeEach(() => {
-    cy.visit(`/`)
-  })
-  it('has a Login button', () => {
+    cy.visit(`/`);
+  });
+  it("has a Login button", () => {
     const button = cy.get(`body > nav > button`);
     button.should(`have.text`, `Login`);
-  })
-  it('redirects to /auth/login when Login button is clicked', () => {
+  });
+  it("redirects to /auth/login when Login button is clicked", () => {
     const button = cy.get(`body > nav > button`);
     button.click();
-    cy.url().should('include', '/auth/login')
-    cy.get('body > div > form > label:nth-child(1)').should('have.text', 'Username:')
-    cy.get('body > div > form > label:nth-child(3)').should('have.text', 'Password:')
+    cy.url().should("include", "/auth/login");
+  });
+
+  describe("/auth/login", () => {
+    it("should have two input text boxes, Username and Password", () => {
+      const login = cy.get(`body > nav > button`);
+      login.click();
+      cy.contains(`E-mail`).should("have.text", "E-mail");
+      cy.contains(`Password`).should('have.text', "Password")
+    });
+    it("should load /auth/register, when register is clicked", () => {
+      cy.visit(`/auth/login`)
+      const registerLink = cy.contains(`Register`);
+      registerLink.click();
+      cy.url().should("include", "register")
+    })
+  });
+
+  describe("/auth/register", ()=> {
+    it("should create a unique user in the DB", () => {
+      cy.visit("/auth/register")
+      const btnEmail = cy.contains("E-mail").get('input#email')
+      btnEmail.type(`jesus${Math.floor(1000 + Math.random() * 9000)}@gmail.com`)
+      const btnUsername = cy.contains("Username").get('input#username')
+      btnUsername.type(`jesus${Math.floor(1000 + Math.random() * 9000)}`)
+      const btnPassword = cy.contains("Password").get('input#password')
+      btnPassword.type("themouse")
+      const btnContinue = cy.contains("Continue");
+      btnContinue.click();
+    })
   })
-})
+});
