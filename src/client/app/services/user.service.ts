a6ba41027d9e30e5fe7d64e90dafc5d05bfdc094
Radoslav Ivanov <riivanov63@gmail.com>
2025-02-22 14:39:46 +0200

    :necktie:(client): Create user; CRUD service client side

---
+++ src/client/app/services/user.service.ts
@@ -0,0 +1,36 @@
+import { IUser } from "@lib/models/user.interface";
+
+const url = `http://localhost:3001`;
+
+export class UserService {
+  static #instance: UserService;
+  private constructor() {}
+
+  public static get instance(): UserService {
+    if (!UserService.#instance) {
+      UserService.#instance = new UserService();
+    }
+
+    return UserService.#instance;
+  }
+
+  // TODO
+  //
+  // CRUD for users
+  //
+
+  async createUser(user: IUser) {
+    console.log(user);
+    const res = await fetch(`${url}/api/v1/user`, {
+      // mode: "no-cors",
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    });
+
+    console.log(res)
+  }
+}
